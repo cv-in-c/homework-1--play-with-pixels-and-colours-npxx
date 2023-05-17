9e4531f90007aa772532e05ed28eb8e2211f84f6
npxx <96121824+npxx@users.noreply.github.com>
2023-05-17 21:24:50 +0530

    Task 1 done

--- src/process_image.c
+++ src/process_image.c
@@ -6,13 +6,25 @@
 
 float get_pixel(image im, int x, int y, int c)
 {
-    // TODO Fill this in
-    return 0;
+    if (x < 0 || x >= im.w || y < 0 || y >= im.h || c < 0 || c >= im.c)
+    {
+        // Implementing the "clamp" padding strategy
+        int xa = (x < 0) ? 0 : im.w;
+        int ya = (y < 0) ? 0 : im.h;
+        int ca = (c < 0) ? 0 : im.c;
+        return im.data[xa + im.w * ya + im.w * im.h * ca];
+    }
+    else
+        return im.data[x + im.w * y + im.w * im.h * c];
 }
 
 void set_pixel(image im, int x, int y, int c, float v)
 {
-    // TODO Fill this in
+    if (x < 0 || x >= im.w || y < 0 || y >= im.h || c < 0 || c >= im.c)
+        return;
+    else
+        im.data[x + im.w * y + im.w * im.h * c] = v;
+    return;
 }
 
 image copy_image(image im)
@@ -40,16 +52,15 @@ void clamp_image(image im)
     // TODO Fill this in
 }
 
-
 // These might be handy
 float three_way_max(float a, float b, float c)
 {
-    return (a > b) ? ( (a > c) ? a : c) : ( (b > c) ? b : c) ;
+    return (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
 }
 
 float three_way_min(float a, float b, float c)
 {
-    return (a < b) ? ( (a < c) ? a : c) : ( (b < c) ? b : c) ;
+    return (a < b) ? ((a < c) ? a : c) : ((b < c) ? b : c);
 }
 
 void rgb_to_hsv(image im)
